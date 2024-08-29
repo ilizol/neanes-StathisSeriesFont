8343aba0315bc693fcdd1869cf13f23d0fb97a88
Elias Zolotas <ilizol@yahoo.gr>
2024-08-29 12:10:33 +0300

    Add font generated images.

---
+++ sbmufl/scripts/generate-images.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import fontforge
+from pathlib import Path
+
+font = fontforge.open("../../NeanesStathisSeries.otf")
+
+
+for glyph in (char for char in font.glyphs() if 57344 <= char.unicode <= 63743):
+    name = glyph.glyphname
+    filename = f"images/{name}/{name}_image_1.png"
+
+    Path(f"images/{name}").mkdir(parents=True, exist_ok=True)
+
+    # font[name].export(filename, 28)
+    # font[name].export(filename, 128)
+    font[name].export(filename, 256)
